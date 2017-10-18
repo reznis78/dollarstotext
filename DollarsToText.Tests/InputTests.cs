4883f1ab8fc6eefb7b1b8fe343e8b74234a600e6
reznis78 <nattyu@sinzer.co.uk>
2017-10-18 18:19:58 +0200

    First drafts of tests

---
+++ DollarsToText.Tests/InputTests.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DollarsToText.Tests
+{
+    [TestClass]
+    public class InputTests
+    {
+        [TestMethod]
+        public void CurrencyInputNull()
+        {
+            string input("");
+
+            CurrencyInputNull(input);
+
+            Assert.IsFalse(CurrencyInput.Success);
+        }
+
+        [TestMethod]
+        public void CurrencyInputTrim()
+        {
+            string input("  1234  ");
+
+            CurrencyInputNull(input);
+
+            Assert.AreEqual(CurrencyInput, 1234);
+        }
+
+        [TestMethod]
+        public void CurrencyInputLettersBase()
+        {
+            string input("21te");
+
+            CurrencyInputNull(input);
+
+            Assert.AreEqual(CurrencyInput, 1234);
+        }
+
+        [TestMethod]
+        public void CurrencyInputLettersSubUnit()
+        {
+            string input("21,4t");
+
+            CurrencyInputNull(input);
+
+            Assert.IsFalse(CurrencyInput.Success);
+        }
+    }
+}
